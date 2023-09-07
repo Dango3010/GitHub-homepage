8b9763f10a663774fcb173edc4ecaa2900201b2b
Oanh Nguyen <oanh@Gradient-MacBook-Air.local>
2023-09-07 10:34:24 -0600

    css a text with link below it with one imaeg in a green dark box

---
+++ src/components/largerComponents/FirstPortion.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles/largerComponentStyle/firstPortionStyle.css';
+
+export default function FirstPortion () {
+
+  return (
+    <div className='firstLayout'>
+    </div>
+  )
+}
